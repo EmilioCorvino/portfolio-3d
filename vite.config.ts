4f4d47549cdd26173a00afb34b74a8669eab8606
Emilio Corvino <e.corvino@reply.it>
2025-02-01 12:15:01 +0100

    Update Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import restart from 'vite-plugin-restart'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    root: 'src/',
+    publicDir: '../public/',
+    plugins: [
+        // Restart server on static/public file change
+        restart({restart: ['../public/**',]}),
+
+        // React support
+        react()
+    ],
+    build: {
+        outDir: '../dist/',
+        emptyOutDir: true,
+        minify: 'esbuild',
+        sourcemap: true,
+    },
 })
